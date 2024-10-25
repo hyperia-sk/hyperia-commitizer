37a4a77a7a23c7ecdd48a82cd312cad97a59b589
Ivošš <ivan.kostolny@hyperia.sk>
2024-10-25 11:43:26 +0200

    new future

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const inquirer = require('inquirer');
 const git = require('simple-git');
 const branchName = require('current-git-branch');
 
-git().add('-A');
-
 module.exports = {
-    prompter: prompter,
-    formatCommit: formatCommit
+    prompter,
+    formatCommit
 };
 
 function prompter(cm, commit) {
@@ -16,91 +14,74 @@ function prompter(cm, commit) {
         {
             type: 'list',
             name: 'type',
-            message: 'Type of operation?',
-            choices: ['task', 'bug', 'fix', 'update', 'merging', 'test', 'refactoring', 'experiment'],
+            message: 'Select the type of change you are committing:',
+            choices: [
+                {value: 'feat', name: 'FEAT - a new feature'},
+                {value: 'fix', name: 'FIX - a bug fix'},
+                {value: 'build', name: 'BUILD - changes that affect the build system or external dependencies'},
+                {value: 'chore', name: 'CHORE - changes to the build process or auxiliary tools'},
+                {value: 'ci', name: 'CI - continuous integration changes'},
+                {value: 'docs', name: 'DOCS - documentation only changes'},
+                {value: 'perf', name: 'PERF - a code change that improves performance'},
+                {value: 'refactor', name: 'REFACTOR - a code change that neither fixes a bug nor adds a feature'},
+                {value: 'revert', name: 'REVERT - reverting a previous commit'},
+                {value: 'style', name: 'STYLE - changes that do not affect the meaning of the code'},
+                {value: 'test', name: 'TEST - adding missing tests or correcting existing tests'},
+                new inquirer.Separator()
+            ],
         },
         {
             type: 'list',
-            name: 'from',
-            message: 'Where did you come from?',
-            choices: ['master', 'beta', new inquirer.Separator(), 'other'],
+            name: 'breakingChange',
+            message: 'It\'s a BREAKING CHANGE:',
+            choices: [
+                {value: 'false', name: 'NO'},
+                {value: 'true', name: 'Yes'},
+                new inquirer.Separator()
+            ],
         },
         {
             type: 'input',
-            name: 'fromManual',
-            message: 'Name of the parent branch:',
-            when: (answers) => answers.from === 'other',
-            validate: function (input) {
-                if (!input) {
-                    return 'LOL ;) You really don\'t know where you came from?';
-                } else {
-                    return true;
-                }
-            }
+            name: 'scope',
+            message: 'Enter the scope of the change (optional):',
+            default: '',
         },
         {
             type: 'input',
-            name: 'workflow',
-            message: 'Job description:',
+            name: 'subject',
+            message: 'Enter a short description of the change:',
             validate: function (input) {
-                if (!input) {
-                    return 'LOL :) You really don\'t know what you were working on???';
-                } else {
-                    return true;
-                }
+                return input ? true : 'LOL :) You really don\'t know what you were working on???';
             }
-        },
-        {
-            type: 'list',
-            name: 'pushing',
-            message: 'Do you want to PUSH automatically?',
-            choices: ['yes', 'no'],
-        },
-    ]).then((answers) => {
-        formatCommit(commit, answers);
-    });
+        }
+    ])
+        .then((answers) => {
+            formatCommit(commit, answers);
+        })
+        .catch(err => {
+            console.error('An error occurred during the prompt', err);
+        });
 }
 
 function formatCommit(commit, answers) {
-    commit(filter([
-        shortBranch(),
-        ': ',
-        answers.workflow,
-        ' [',
-        answers.type,
-        ' from ',
-        (answers.fromManual ? answers.fromManual : answers.from),
-        '] (',
-        branchName(),
-        ')'
-    ]).join(''));
+    const scopePart = answers.scope ? `(${answers.scope})` : '';
+    const breakingChange = answers.breakingChange === 'true' ? '!' : '';
+    const commitMessage = `${answers.type}${scopePart}${breakingChange}: ${answers.subject} [${shortBranch()}]`;
 
-    if(answers.pushing === 'yes'){
-        git()
-            .push('origin', 'HEAD')
-            .then(() => {
-                console.log('\n\n    - - Pushing to Repo finished SUCCESSFULLY - -\n\n');
-            })
-            .catch((err) => {
-                console.error('\n\n    ! ! ! Pushing to Repo FAILED ! ! !\n\n\n', err);
-            });
+    try {
+        commit(commitMessage)
+    } catch (err) {
+        console.error('\n\n    ! ! ! Commit FAILED ! ! !\n\n\n', err);
     }
 }
 
 function shortBranch() {
-    var name = branchName();
+    const name = branchName();
 
-    if(name.includes("task/")){
-        var format = name.split('/')[1].split('-');
-
+    if (name.includes("task/")) {
+        const format = name.split('/')[1].split('-');
         return format[0] + '-' + format[1];
     }
 
     return name;
-}
-
-function filter(array) {
-    return array.filter(function(item) {
-        return !!item;
-    });
 }
